51df1e21f4bc326649f5aed5d249bdb3a6669594
Waters <166139510+Waters-P@users.noreply.github.com>
2024-05-26 21:02:09 +0000

    init_commit

--- app.rb
+++ app.rb
@@ -2,8 +2,63 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:squaring_method)
+  
+end
+
+
+get("/square/new") do
+
+
+
+  erb(:squaring_method)
+end
+
+get("/square/results") do
+
+
+  erb(:squaring_results)
+
+end
+
+
+get("/square_root/new") do
+
+
+
+  erb(:square_root_method)
+end
+
+get("/square_root/results") do
+
+
+  erb(:square_root_results)
+
+end
+
+
+
+get("/payment/new") do
+
+
+
+  erb(:payment_method)
+end
+
+get("/payment/results") do
+
+
+
+  erb(:payment_results)
+end
+
+
+
+
+get("/random/new") do
+
+
+
+  erb(:random_method)
 end
